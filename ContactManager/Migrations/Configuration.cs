48230dc271e1eb1cc69c4c6ec39f8fe4e6791c48
longhai <lcyvb@mail.missouri.edu>
2015-08-01 23:50:14 +0000

    Using the Membership API

--- ContactManager/Migrations/Configuration.cs
+++ ContactManager/Migrations/Configuration.cs
@@ -5,9 +5,30 @@ namespace ContactManager.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ContactManager.Models.ApplicationDbContext>
     {
+        bool AddUserAndRole(ContactManager.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            ir = rm.Create(new IdentityRole("canEdit"));
+            var um = new UserManager<ApplicationUser>
+                (new UserStore<ApplicationUser>(context));
+            var user = new ApplicationUser()
+            {
+                UserName = "longhai",
+            };
+            ir = um.Create(user, "longhai");
+            if (ir.Succeeded == false)
+                return ir.Succeeded;
+            ir = um.AddToRole(user.Id, "canEdit");
+            return ir.Succeeded;
+        }
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -28,6 +49,7 @@ namespace ContactManager.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            AddUserAndRole(context);
             context.Contacts.AddOrUpdate( c => c.Name,
                 new Contact
                 {
